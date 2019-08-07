1f293cc89f843106ebfbc551bdb2c9d5d3d1d473
Dimitri Lozeve <dimitri@lozeve.com>
2019-08-07 23:49:01 +0200

    Project the grid on the plane and plot the results

--- projectile.rkt
+++ projectile.rkt
@@ -1,5 +1,5 @@
 #lang racket
-
+(require racket/draw)
 
 (define (dot x y)
   (for/sum ([i x]
@@ -17,10 +17,6 @@
   (origin u v)
   #:transparent)
 
-(define (make-plane origin u v)
-  ;; TODO pad with zeros if dimension mismatch
-  (plane origin (normalize u) (normalize v)))
-
 ;; Position of a point relative to a plane origin point
 (define (affine-transform plane point)
   (for/vector ([i point]
@@ -31,7 +27,7 @@
   (let* ([new-point (affine-transform plane point)]
 	 [point-u (dot new-point (plane-u plane))]
 	 [point-v (dot new-point (plane-v plane))])
-    #(point-u point-v)))
+    (vector point-u point-v)))
 
 (define (distance plane point)
   (let* ([new-point (affine-transform plane point)]
@@ -52,11 +48,46 @@
 		      (+ i j)))])
     (apply max (for/list ([c corners]) (distance plane c)))))
 
-(define (neighbours grid-point)
-  (let ([n (vector-length grid-point)])
-    (for*/list ([k '(-1 1)]
-		[i (range n)])
-      (let ([new-grid-point (vector-copy grid-point)])
-	(vector-set! new-grid-point i (+ k (vector-ref new-grid-point i)))
-	new-grid-point))))
+(define (make-grid dimension size)
+  (apply append
+	 (for/list ([u (apply cartesian-product (build-list dimension (lambda _ (range size))))])
+	   (for/list ([k (in-range dimension)])
+	     (let ([v (apply vector u)])
+	       (vector-set! v k (add1 (vector-ref v k)))
+	       (list (apply vector u) v))))))
 
+(define (project-grid-on-plane plane grid)
+  (let ([d (hypercube-width plane)])
+    (for/list ([u grid]
+	       #:when (and (> d (distance plane (car u)))
+			   (> d (distance plane (cadr u)))))
+      (list (project plane (car u)) (project plane (cadr u))))))
+
+(define (draw-tiling tiling size)
+  (define target (make-bitmap size size))
+  (define dc (new bitmap-dc% [bitmap target]))
+  (send dc set-pen "white" 1 'solid)
+
+  (define max-pos
+    (apply max (apply append (for/list ([s tiling])
+			       (append (vector->list (car s))
+				       (vector->list (cadr s)))))))
+
+  (define factor (/ size max-pos))
+
+  (for ([segment tiling])
+    (match segment
+      [(list (vector a b) (vector c d))
+       (send dc draw-line (* factor a) (* factor b) (* factor c) (* factor d))]))
+
+  target)
+
+(define phi (/ (+ 1. (sqrt 5)) 2))
+
+(define p (plane (vector 0. 0. 0 1. 0.)
+		 (vector phi 0 (- phi) -1 1)
+		 (vector -1 1 phi 0 (- phi))))
+
+;; (define p (plane (make-vector 3 0.) (vector 0. 0. 1.) (vector 0. 1. 0.)))
+(define tiling (project-grid-on-plane p (make-grid 3 5)))
+(draw-tiling tiling 500)
